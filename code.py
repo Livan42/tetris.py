2e07753d488fb70d2cc30479fdf29d697d0649d4
Livan42 <120690374+Livan42@users.noreply.github.com>
2023-01-10 23:44:13 +0100

    Update code.py

--- code.py
+++ code.py
@@ -48,3 +48,21 @@ def apply_tetromino(self):
         self.level = self.total_lines_eliminated // 10
         self.reset_tetromino()
 
+ def get_color(self, r, c):
+        return self.tetromino_color if (r, c) in self.get_tetromino_coords() else self.field[r][c]
+ 
+ def is_cell_free(self, r, c):
+        return r < Tetris.FIELD_HEIGHT and 0 <= c < Tetris.FIELD_WIDTH and (r < 0 or self.field[r][c] == 0)
+    
+ def move(self, dr, dc):
+     with self.move_lock:
+         if self.game_over:
+             return
+
+         if all(self.is_cell_free(r + dr, c + dc) for (r, c) in self.get_tetromino_coords()):
+             self.tetromino_offset = [self.tetromino_offset[0] + dr, self.tetromino_offset[1] + dc]
+         elif dr == 1 and dc == 0:
+                self.game_over = any(r < 0 for (r, c) in self.get_tetromino_coords())
+                if not self.game_over:
+                    self.apply_tetromino()
+
