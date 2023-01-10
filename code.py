8aa96e00f946008d1adc4400472d3d7bd95daf78
Livan42 <120690374+Livan42@users.noreply.github.com>
2023-01-10 23:33:12 +0100

    Update code.py

--- code.py
+++ code.py
@@ -3,3 +3,17 @@ import random
 from threading import Lock
 
 COLORS = ['gray', 'lightgreen', 'pink', 'blue', 'orange', 'purple']
+
+class Tetris():
+    FIELD_HEIGHT = 20
+    FIELD_WIDTH = 10
+    SCORE_PER_ELIMINATED_LINES = (0, 40, 100, 300, 1200)
+    TETROMINOS = [
+        [(0, 0), (0, 1), (1, 0), (1,1)], # O
+        [(0, 0), (0, 1), (1, 1), (2,1)], # L
+        [(0, 1), (1, 1), (2, 1), (2,0)], # J 
+        [(0, 1), (1, 0), (1, 1), (2,0)], # Z
+        [(0, 1), (1, 0), (1, 1), (2,1)], # T
+        [(0, 0), (1, 0), (1, 1), (2,1)], # S
+        [(0, 1), (1, 1), (2, 1), (3,1)], # I
+    ]
